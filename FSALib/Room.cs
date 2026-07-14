f6f01ea5f66229bf5f640e3938fcc0b5a40e9d5d
Venomalia <87765258+Venomalia@users.noreply.github.com>
2026-07-14 15:07:54 +0200

    Add RARC serialization to Room class

--- FSALib/Room.cs
+++ FSALib/Room.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AuroraLib.Core.Format.Identifier;
+using System;
+using System.IO;
+using static FSALib.Rarc;
 
 namespace FSALib
 {
@@ -14,7 +17,7 @@ namespace FSALib
         /// <summary>
         /// A collection of actors in the room (e.g., players, NPCs, enemies, etc.)
         /// </summary>
-        public readonly ActorList Actors = new ActorList();
+        public readonly ActorList Actors;
 
         /// <summary>
         /// The index of the room. This could be used to identify or reference the room in the game world.
@@ -40,9 +43,80 @@ namespace FSALib
 
         public Room()
         {
+            Actors = new ActorList();
             for (int i = 0; i < _layers.Length; i++)
             {
                 _layers[i] = new Layer();
+            }
+        }
+
+        /// <summary>
+        /// Loads the room actor data and all layer data from a RARC archive.
+        /// </summary>
+        /// <param name="root">The root directory of the RARC archive.</param>
+        /// <param name="mapIndex">The stage map index.</param>
+        /// <param name="roomIndex">The room index within the stage.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown if the room actor file could not be found in the archive.
+        /// </exception>
+        public void LoadFromRarc(DirectoryNode root, int mapIndex, int roomIndex)
+        {
+            DirectoryNode actorDirectory = root.Directorys[Rarc.CommonFolderTypes.BIN].Directorys[(Identifier32)$"B{mapIndex:D3}"];
+            DirectoryNode layerDirectory = root.Directorys[Rarc.CommonFolderTypes.SZS].Directorys[(Identifier32)$"M{mapIndex:D3}"];
+
+            string actorFileName = ActorList.GetFileName(mapIndex, roomIndex);
+
+            if (!actorDirectory.TryGetFile(actorFileName, out FileNode actorFile))
+                throw new ArgumentException($"The room actor file '{actorFileName}' was not found.");
+
+            Actors.ReadFromStream(actorFile.Data);
+            LoadLayers(layerDirectory, mapIndex, roomIndex);
+        }
+
+        internal void LoadLayers(DirectoryNode layerDirectory, int mapIndex, int roomIndex)
+        {
+            for (int i = 0; i < Layers.Length; i++)
+            {
+                int layerIndex = i % 8;
+                int layerLevel = i / 8 + 1;
+                string layerFileName = Layer.GetFileName(mapIndex, roomIndex, layerLevel, layerIndex);
+
+                if (layerDirectory.TryGetFile(layerFileName, out FileNode layerFile))
+                    Layers[i].ReadFromStream(layerFile.Data);
+            }
+        }
+
+        /// <summary>
+        /// Saves the room actor data and all layer data to a RARC archive.
+        /// </summary>
+        /// <param name="root">The root directory of the RARC archive.</param>
+        /// <param name="mapIndex">The stage map index.</param>
+        /// <param name="roomIndex">The room index within the stage.</param>
+        public void SaveToRarc(DirectoryNode root, int mapIndex, int roomIndex)
+        {
+            var actorFolderkey = root.CreateDirectory(Rarc.CommonFolderTypes.BIN).CreateDirectory((Identifier32)$"B{mapIndex:D3}");
+            var layerFolderkey = root.CreateDirectory(Rarc.CommonFolderTypes.SZS).CreateDirectory((Identifier32)$"M{mapIndex:D3}");
+
+            string actorFileName = ActorList.GetFileName(mapIndex, roomIndex);
+            var actorData = actorFolderkey.GetFile(actorFileName, FileMode.Create).Data;
+            Actors.WriteToStream(actorData);
+
+            SaveLayers(layerFolderkey, mapIndex, roomIndex);
+        }
+
+        internal void SaveLayers(DirectoryNode layerDirectory, int mapIndex, int roomIndex)
+        {
+            for (int i = 0; i < Layers.Length; i++)
+            {
+                int layerIndex = i % 8;
+                int layerLevel = i / 8 + 1;
+                string layerFileName = Layer.GetFileName(mapIndex, roomIndex, layerLevel, layerIndex);
+
+                var layerFile = layerDirectory.GetFile(layerFileName, FileMode.Create);
+                layerFile.Attribute |= FileAttribute.YAZ0_COMPRESSED;
+                var layerData = layerFile.Data;
+                layerData.SetLength(0);
+                Layers[i].WriteToStream(layerData);
             }
         }
 
